ab6cca487816de5783ed0f6f14dd9919142849b7
Tiago Barufi <barufi@gmail.com>
2012-11-23 17:48:47 -0200

    peganingas

--- dedo.py
+++ dedo.py
@@ -4,13 +4,12 @@ from PyQt4 import QtCore, QtGui, QtWebKit
 from PyQt4.QtCore import Qt, SIGNAL, QEvent, QPoint, QSize, QUrl, QThread, QTimer, QPropertyAnimation, QRect,  QProcess,  SLOT
 from finger import Ui_MainWindow
 import Image;
-from PyQt4.QtOpenGL import *
-from OpenGL.GL import *
-from OpenGL.GLU import *
-from shader import ShaderProgram,ShaderCode
+
 
 from multiprocessing import Process
 class Rabisk(QtGui.QMainWindow):
+    text="peganingas"
+    pic=None
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
@@ -19,88 +18,46 @@ class Rabisk(QtGui.QMainWindow):
         self.layers = []
         self.canvasWidth = 640 # size of image, not entire widget
         self.canvasHeight = 480
-        self.initializeGL()
-        
+        self.ui.label.setGeometry(QtCore.QRect(0, 0, 66, 17))
+        print self.ui.centralwidget.width
+        #self.ui.label.geometry.Width=self.ui.geometry.Width
+        #self.ui.label.height=self.ui.height
         self.setWindowState(Qt.WindowFullScreen)
+        print self.ui.centralwidget.width()
+        self.ui.label.setGeometry(QtCore.QRect(0, 0, self.ui.centralwidget.width(), self.ui.centralwidget.height()))
+        self.pic=QPixmap(self.ui.centralwidget.width(), self.ui.centralwidget.height())
         self.camera=QProcess(self)
         self.camera.readyReadStandardOutput.connect(self.handle_stdout)
         self.camera.start('python sense.py')
+        self.show()
         
         
+        
+    def paintEvent(self, event):
+        print "drawing"    
+        qp=QtGui.QPainter()
+        qp.begin(self)
+        qp.setPen(QtGui.QColor(1,10,0))
+        qp.drawText(10,10, self.text)
+        qp.end()
         
         
     def handle_stdout(self):
         byteArray=self.camera.readAllStandardOutput()
         data=re.sub(r'''\n+$''', "", byteArray.data())
         print data
-        self.paintGL()
+        self.text=data
+        #self.ui.label.setText("whatever"+data);
+        qp=QtGui.QPainter(self.pic)
+        qp.setPen(QtGui.QColor(1,10,0))
+        qp.drawText(100,10, self.text)
+        self.ui.label.setPixmap(self.pic)
+        
+
     def keyPressEvent(self, e):
         k=e.key()
         print e.key()
-    def initializeGL(self):
-        glClearColor(0.4, 0.4, 0.4, 1.0)
 
-        # if a starting image, load it
-#        if self.startImg is not None:
-#            print(self.img.width())
-
-        self.layers = [ Layer(self.canvasWidth,self.canvasHeight) ]
-        self.strokeLayer = Layer(self.canvasWidth,self.canvasHeight)
-
-        # if there's a starting image, draw it into the starting layer
-        """
-        if self.imgPath is not None:
-            bottomLayer = self.layers[-1]
-            bottomLayer.bind()
-
-            # setup the orthographic projection and viewport
-            glMatrixMode(GL_PROJECTION)
-            glPushMatrix()
-            glLoadIdentity()
-            glOrtho(0, self.canvasWidth, 0, self.canvasHeight, -1, 1)
-            glViewport(0, 0, self.canvasWidth, self.canvasHeight)
-            
-
-            imgTextureId = self.bindTexture(self.startImg, GL_TEXTURE_RECTANGLE_ARB)
-            self.drawTexture(QPointF(0,0), imgTextureId, GL_TEXTURE_RECTANGLE_ARB)
-
-            self.deleteTexture(imgTextureId)
-
-            # restore the previous projection matrix and viewport
-            glMatrixMode(GL_PROJECTION)
-            glPopMatrix()
-            glViewport(0, 0, self.width(), self.height()) # should be saved off?
-
-            bottomLayer.release()
-
-            self.startImg = None
-        """
-        # setup the necessary shaders
-        self.program = ShaderProgram()
-        self.program.attachShader(ShaderCode(GL_VERTEX_SHADER,'./shaders/basic.vert'))
-        self.program.attachShader(ShaderCode(GL_FRAGMENT_SHADER,'./shaders/basic.frag'))
-    def paintGL(self):
-        glClear(GL_COLOR_BUFFER_BIT)
-
-        # draw FBO layers from the bottom up
-        for layer in reversed(self.layers):
-            self.drawTexture(QPointF(0,0), layer.texture(), GL_TEXTURE_RECTANGLE_ARB)
-
-        # if stroke, draw to the stroke layer, draw stroke layer
-        self.program.bind()
-        glBegin(GL_QUADS)
-        glVertex2f(10,10)
-        glVertex2f(20,10)
-        glVertex2f(20,20)
-        glVertex2f(10,20)
-        glEnd()
-        self.program.release()
-class Layer(QGLFramebufferObject):
-    def __init__(self, width, height, target=GL_TEXTURE_RECTANGLE_ARB):
-        QGLFramebufferObject.__init__(self, width, height, 
-                                      QGLFramebufferObject.CombinedDepthStencil,
-                                      target,
-                                      GL_RGBA8)
 app = QtGui.QApplication(sys.argv)
 myapp = Rabisk() 
 myapp.show()
--- finger.py
+++ finger.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'finger.ui'
 #
-# Created: Fri Nov  9 14:59:09 2012
+# Created: Thu Nov 22 15:46:08 2012
 #      by: PyQt4 UI code generator 4.9.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -17,10 +17,30 @@ except AttributeError:
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(800, 600)
+        MainWindow.resize(916, 723)
         self.centralwidget = QtGui.QWidget(MainWindow)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Maximum, QtGui.QSizePolicy.Maximum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.centralwidget.sizePolicy().hasHeightForWidth())
+        self.centralwidget.setSizePolicy(sizePolicy)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.gridLayoutWidget = QtGui.QWidget(self.centralwidget)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(0, 0, 901, 711))
+        self.gridLayoutWidget.setObjectName(_fromUtf8("gridLayoutWidget"))
+        self.gridLayout_2 = QtGui.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout_2.setMargin(0)
+        self.gridLayout_2.setObjectName(_fromUtf8("gridLayout_2"))
+        self.label_2 = QtGui.QLabel(self.gridLayoutWidget)
+        self.label_2.setText(_fromUtf8(""))
+        self.label_2.setPixmap(QtGui.QPixmap(_fromUtf8("NightCrawler.jpg")))
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.gridLayout_2.addWidget(self.label_2, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
+        self.menuBar = QtGui.QMenuBar(MainWindow)
+        self.menuBar.setGeometry(QtCore.QRect(0, 0, 916, 25))
+        self.menuBar.setObjectName(_fromUtf8("menuBar"))
+        MainWindow.setMenuBar(self.menuBar)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
